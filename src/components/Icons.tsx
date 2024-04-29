1ba22ee05a01271d4e597be26fe9880621bddd9f
Adrian Lach <adrian.lach1999@gmail.com>
2024-04-29 23:33:19 +0200

    Added Admin Icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -152,4 +152,16 @@ const BinIcon = (props: IconProps) => {
     )
 }
 
-export { HomeIcon, HomeSolidIcon, CheckCircleIcon, CheckCircleSolidIcon, SettingsIcon, SettingsSolidIcon, LinkIcon, ProfileCardIcon, ProfileCardSolidIcon, BinIcon, BinSolidIcon, CloseIcon }
+const AdminIcon = (props: IconProps) => {
+
+    const { size, className } = parseIconProps(props)
+
+    return (
+        <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 16H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v1M9 12H4m8 8V9h8v11h-8Zm0 0H9m8-4a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"/>
+        </svg>
+
+    )
+}
+
+export { HomeIcon, HomeSolidIcon, CheckCircleIcon, CheckCircleSolidIcon, SettingsIcon, SettingsSolidIcon, LinkIcon, ProfileCardIcon, ProfileCardSolidIcon, BinIcon, BinSolidIcon, CloseIcon, AdminIcon }
