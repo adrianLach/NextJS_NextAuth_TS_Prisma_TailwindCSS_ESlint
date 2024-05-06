203f15a0beab9f0341fe5596e502a1314915f9b5
Adrian Lach <adrian.lach1999@gmail.com>
2024-05-06 13:26:30 +0200

    Fix text-white

--- src/app/apps/layout.tsx
+++ src/app/apps/layout.tsx
@@ -11,7 +11,7 @@ export default function AppsLayout({
         
         return <Link href={href} className='flex flex-row items-center gap-4 hover:bg-slate-600 hover:rounded-md p-2 transition-all'>
             {icon}
-            <p className='text-xl dark:text-white md:block hidden'>{name}</p>
+            <p className='text-xl text-white md:block hidden'>{name}</p>
         </Link>
     }
 
@@ -20,18 +20,18 @@ export default function AppsLayout({
             <div className='flex flex-col bg-slate-800 h-full'>
                 <div className='h-16 bg-slate-800 w-full flex flex-col justify-center pl-4'>
                     <Link href={'/apps'}  className='flex flex-row items-end'>
-                        <HomeSolidIcon size={48} className='dark:text-white'></HomeSolidIcon>
-                        <p className='text-3xl dark:text-white font-bold md:block hidden'>Arco</p>
+                        <HomeSolidIcon size={48} className='text-white'></HomeSolidIcon>
+                        <p className='text-3xl text-white font-bold md:block hidden'>Arco</p>
                     </Link>
                 </div>
                 <nav className='h-full flex flex-col gap-2 justify-start p-4'>
-                    <MenuItem href='/apps/shortcuts' icon={<LinkIcon size={32} className='dark:text-white'></LinkIcon>} name='Shortcuts'></MenuItem>
-                    <MenuItem href='/apps/tasks' icon={<CheckCircleIcon size={32} className='dark:text-white'></CheckCircleIcon>} name='Tasks'></MenuItem>
+                    <MenuItem href='/apps/shortcuts' icon={<LinkIcon size={32} className='text-white'></LinkIcon>} name='Shortcuts'></MenuItem>
+                    <MenuItem href='/apps/tasks' icon={<CheckCircleIcon size={32} className='text-white'></CheckCircleIcon>} name='Tasks'></MenuItem>
                     <div className='grow'></div>
                     <div className='h-[2px] border border-slate-500'></div>
-                    <MenuItem href='/account' icon={<ProfileCardIcon size={32} className='dark:text-white'></ProfileCardIcon>} name='Account'></MenuItem>
-                    <MenuItem href='/apps' icon={<SettingsIcon size={32} className='dark:text-white'></SettingsIcon>} name='Settings'></MenuItem>
-                    <MenuItem href='/apps' icon={<AdminIcon size={32} className='dark:text-white'></AdminIcon>} name='Admin'></MenuItem>
+                    <MenuItem href='/account' icon={<ProfileCardIcon size={32} className='text-white'></ProfileCardIcon>} name='Account'></MenuItem>
+                    <MenuItem href='/apps' icon={<SettingsIcon size={32} className='text-white'></SettingsIcon>} name='Settings'></MenuItem>
+                    <MenuItem href='/apps' icon={<AdminIcon size={32} className='text-white'></AdminIcon>} name='Admin'></MenuItem>
                 </nav>
             </div>
             <div className='flex w-full flex-col h-full'>
